bd6450e5056647218ec9c1b07c0922e6767f7049
Bartosz Jasiński <bartosz.jasinski2.stud@pw.edu.pl>
2023-01-24 00:33:14 +0100

    Opis metod w klasie Order

--- src/Orders.h
+++ src/Orders.h
@@ -27,13 +27,29 @@ class Order
     public:
         Order(int orderId_, Date dateOfOrder_, Book book_, int quantity_, Customer whoOrdered_):
         orderId(orderId_), dateOfOrder(dateOfOrder_), book(book_), quantity(quantity_), whoOrdered(whoOrdered_) {}
-
+        /**
+         * Metoda zwracająca id zamówienia
+        */
         int getOrderId() const;
+        /**
+         * Metoda zwracająca date złożenia zamówienia
+        */
         Date getDateOfOrder() const;
+        /**
+         * Metoda zwracająca książke która została zamówiona
+        */
         Book getOrderedBook() const;
+        /**
+         * Metoda zwracająca ile sztuk danej książki zostało zamówione
+        */
         int getQuantity() const;
+        /**
+         * Metoda zwracająca klienta, który złożył dane zamówienie
+        */
         Customer getCustomer() const;
-
+        /**
+         * Metoda ustawiająca ilość danej książki w złożonym zamówieniu
+        */
         void setQuantity(int quantity_);
         
 };
